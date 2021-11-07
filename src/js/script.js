9267a5998c5a5957eee6cab7627c557ce327b988
Agata Szulejko <amoranska@poczta.wp.pl>
2021-11-07 17:28:13 +0100

    Add prepareCartProductParams and prepareCartProduct methods

--- src/js/script.js
+++ src/js/script.js
@@ -207,6 +207,7 @@
             price *= thisProduct.amountWidget.value;
 
             // update calculated price in the HTML
+            thisProduct.priceSingle = thisProduct.priceElem.innerHTML;
             thisProduct.priceElem.innerHTML = price;
 
         }
@@ -228,9 +229,64 @@
             thisProduct.cartButton.addEventListener('click', function(event) {
                 event.preventDefault();
                 thisProduct.processOrder();
+                thisProduct.addToCart();
             });
         }
-    }
+        addToCart() {
+            const thisProduct = this;
+            app.cart.add(thisProduct.prepareCartProduct());
+        }
+
+        prepareCartProduct() {
+            const thisProduct = this;
+            const productSummary = {
+                  id: thisProduct.id,
+
+                  name: thisProduct.data.name,
+                  amount: thisProduct.amountWidget.value,
+
+                  priceSingle: thisProduct.priceSingle,
+                  price: thisProduct.amountWidget.value * thisProduct.priceSingle,
+                  params: thisProduct.prepareCartProductParams(),
+           };
+
+          return productSummary;
+        }
+
+        prepareCartProductParams() {
+
+            const thisProduct = this;
+
+            // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+            const formData = utils.serializeFormToObject(thisProduct.form);
+
+            const params = {};
+              // for every category (param)...
+            for (let paramId in thisProduct.data.params) {
+              // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+                const param = thisProduct.data.params[paramId];
+
+                // create category param in params const eg. params = { ingredients: { name: 'Ingredients', options: {}}}
+                params[paramId] = {
+                  label: param.label,
+                  options: {}
+                }
+
+                // for every option in this category
+            for (let optionId in param.options) {
+                // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+                const option = param.options[optionId];
+
+                const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+
+                if(optionSelected) {
+                params[paramId].options[optionId] = option.label;
+                }
+              }
+            }
+                return params;
+          }
+      }
 
     class AmountWidget {
         constructor(element) {
@@ -319,6 +375,11 @@
             thisCart.dom.toggleTrigger.addEventListener('click', function() {
             thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
             });
+        }
+
+        add(menuProduct) {
+     // const thisCart = this;
+        console.log('adding product', menuProduct);
         }
 
 
